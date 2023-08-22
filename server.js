d9dee73bdc86db0557e2495dfd9d3d36446e0a13
Maureen <maurenjimenezr@gmail.com>
2023-08-22 17:05:12 -0600

    Ready

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+const { faker } = require('@faker-js/faker');
+
+class Usuario {
+    constructor() {
+        this._id = faker.string.uuid(),
+        this.nombre = faker.person.firstName(),
+        this.apellido = faker.person.lastName(),
+        this.telefono = faker.phone.number(),
+        this.email = faker.internet.email()
+        this.password = faker.internet.password()
+    }
+}
+
+class Empresa {
+    constructor() {
+        this._id = faker.string.uuid(),
+        this.nombre = faker.company.name(),
+        this.direccion = {
+            calle: faker.location.street(),
+            ciudad: faker.location.city(),
+            estado: faker.location.state(),
+            cp: faker.location.zipCode(),
+            pais: faker.location.country()
+        }
+    }
+}
+
+app.get("/api/users/new", (req, res) => {
+    let nuevoUsuario = new Usuario();
+    res.json(nuevoUsuario);
+})
+
+app.get("/api/companies/new", (req, res) => {
+    let nuevaEmpresa = new Empresa();
+    res.json(nuevaEmpresa);
+})
+
+app.get("/api/company/new", (req, res) => {
+    let usuario = new Usuario();
+    let empresa = new Empresa();
+    let usuarioEmpresa = [usuario, empresa];
+    res.json(usuarioEmpresa);
+})
+
+
+
+app.listen(8000, () => console.log("Servidor corriendo") );
